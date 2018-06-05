92d6ebdbc172f1ede0392953678defaeb81d413c
Dagzy <quincye.williams@gmail.com>
2018-06-05 13:55:04 -0400

    lots of changes, pushing to format

--- EFConnect.API/Controllers/AuthController.cs
+++ EFConnect.API/Controllers/AuthController.cs
@@ -1,5 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using EFConnect.Contracts;
+using EFConnect.Data.Entities;
+using EFConnect.Models.User;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EFConnect.API.Controllers
 {
@@ -7,10 +16,66 @@ namespace EFConnect.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
-        public AuthController(IAuthService authService)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthService authService, IConfiguration config)
         {
+            _config = config;
             _authService = authService;
 
         }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegister userForRegister)
+        {
+            if (!string.IsNullOrEmpty(userForRegister.Username))
+                userForRegister.Username = userForRegister.Username.ToLower();
+
+            if (await _authService.UserExists(userForRegister.Username))
+                ModelState.AddModelError("Username", "Username already exists");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            userForRegister.Username = userForRegister.Username.ToLower();
+
+            if (await _authService.UserExists(userForRegister.Username))
+                return BadRequest("Username is already taken");
+
+            var userToCreate = new User
+            {
+                Username = userForRegister.Username
+            };
+            var createUser = await _authService.Register(userToCreate, userForRegister.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForLogin userForLogin)
+        {
+            var userFromDb = await _authService.Login(userForLogin.Username.ToLower(), userForLogin.Password);
+
+            if (userFromDb == null)
+                return Unauthorized();
+
+            //Generate Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(ClaimTypes.NameIdentifier, userFromDb.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromDb.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { tokenString });
+        }
+
     }
 }
--- EFConnect.API/Startup.cs
+++ EFConnect.API/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using EFConnect.Contracts;
 using EFConnect.Data;
 using EFConnect.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EFConnect.API
 {
@@ -29,9 +32,20 @@ namespace EFConnect.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
             services.AddDbContext<EFConnectContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddScoped<IAuthService, AuthService>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +59,7 @@ namespace EFConnect.API
             {
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
